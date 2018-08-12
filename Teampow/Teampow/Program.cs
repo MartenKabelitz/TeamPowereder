b4e478f5f7d9d44daeb0eabe232ad84f1c0ed97e
Marten Kabelitz <martenkabel@gmail.com>
2018-08-12 10:14:49 +0200

    frist commit

---
+++ Teampow/Teampow/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Teampow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

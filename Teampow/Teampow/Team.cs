327dced6c6af4624090a0a2eb4e3235a78c40c0d
Marten Kabelitz <martenkabel@gmail.com>
2018-08-12 10:47:24 +0200

    Added two files

---
+++ Teampow/Teampow/Team.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Teampow
+{
+    class Team
+    {
+        public void tem()
+        {
+            Console.WriteLine("tem is shit");
+        }
+    }
+}
